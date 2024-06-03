588e7170de66d098077f76d8011e3dc4cf9ec932
K Mohammad <95924696+kattubadimohammad@users.noreply.github.com>
2024-06-03 12:08:30 +0530

    Update setup.py

--- Book Recommendation System/setup.py
+++ Book Recommendation System/setup.py
@@ -1,26 +1,24 @@
-## Import Statements:
 from setuptools import setup
 
-## Readme File Handling:
+# Readme File Handling:
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-## edit below variables as per your requirements -
-REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "K Mohammad"
-SRC_REPO = "src"
+# Edit below variables as per your requirements
+REPO_NAME = "Data-Science-Projects"
+AUTHOR_USER_NAME = "kattubadimohammad"
+SRC_REPO = "Book Recommendation System"
 LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
 
-## Setup Function:
-
+# Setup Function:
 setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for Movie Recommender System",
+    description="A small package for Book Recommendation System",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/tree/my-new-branch/{SRC_REPO}",
     author_email="miraclemohammad786@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
